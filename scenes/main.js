408db30559d44ec7c1b76867e65a42699836b64b
JuanCedeno96 <>
2021-06-12 05:10:21 +0000

    move right and left, enemy sprite

--- scenes/main.js
+++ scenes/main.js
@@ -1,18 +1,31 @@
-add([
+const player = add([
   sprite('player'),
   scale(0.5),
-  pos(20,20)
+  pos(20,20),
+  body()
 ])
+
+const MOVE_SPEED = 200
+
+keyDown('right', ()=>{
+  player.move(MOVE_SPEED,0)
+})
+
+keyDown('left', ()=>{
+  player.move(-MOVE_SPEED,0)
+})
 
 addLevel([
   '      ',
   '   @  ',
-  '      ',
-  '      ',
-  'xxxxxx',
+  '     ',
+  
+  'xxxxxxxx',
 
 ], {
   width: 40,
   height: 40,
-  'x': [sprite('ground')]
+  'x': [sprite('ground'), solid()],
+  '@': [sprite('enemy'), scale(0.5)]
+
 })
